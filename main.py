e9b658e05fc55c699cc745e5f2ab99856db73cf9
Adam Saul <adamsaul314@gmail.com>
2026-06-05 23:40:02 +0100

    updated move functionality

--- main.py
+++ main.py
@@ -15,9 +15,8 @@ print(dir_list)
 fileToFolder = {
     "pdf": "Documents",
     "txt": "Documents",
-    "pdf": "Documents",
     "webp": "Images",
-    "jpg": "Images"
+    "jpg": "Images",
 }
 
 os.makedirs(documentPath, exist_ok=True)
@@ -26,23 +25,18 @@ os.makedirs(imagePath, exist_ok=True)
 for filename in dir_list:
     if filename.endswith(".pdf") or filename.endswith(".txt"):
         try:
-                shutil.move(current_directory, documentPath)
-                print("Files sorted into Documents directory!")
+            shutil.move(current_directory, documentPath)
+            print("Files sorted into Documents directory!")
         except FileExistsError:
-                print("Duplicate Files Present")
+            print("Duplicate Files Present")
         break
-
-
-
 
 
 for filename in dir_list:
     if filename.endswith(".webp") or filename.endswith(".jpg"):
         try:
-                shutil.move(current_directory, imagePath)
-                print("Files sorted into Images directory!")
+            shutil.move(current_directory, imagePath)
+            print("Files sorted into Images directory!")
         except FileExistsError:
-                print("Duplicate Files Present")
+            print("Duplicate Files Present")
         break
-            
-        
