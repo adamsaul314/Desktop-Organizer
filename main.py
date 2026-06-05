7e0065ddac9c20d88da130b680c1f0efdd5a8942
Adam Saul <adamsaul314@gmail.com>
2026-06-05 22:30:09 +0100

    printing contents of target directory

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 
-print(os.getcwd())
-
 os.chdir("C:\\Users\\adams\\Desktop\\Desktop-Organizer\\Test_Desktop")
 
-print(os.getcwd())
+current_directory = os.getcwd()
+
+dir_list = os.listdir(current_directory)
+
+print(dir_list)
+
